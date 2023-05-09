31432b7fb13993f707ce748b63070ff0ae7b67ec
JoviTech <developerlucaslima@gmail.com>
2023-05-09 00:41:33 -0300

    create transactions routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,28 @@
+import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import randomUUID from 'node:crypto'
 
-export async function transactionsRoutes(app) {
-  app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, response) => {
+    // {title, amount, type: credit or debit}
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return tables
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID,
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      type,
+    })
+
+    return response.status(201).send()
   })
 }
