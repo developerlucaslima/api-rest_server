50d6ea5c14f86ab3c52a928f98a972e9c4fd87e5
JoviTech <developerlucaslima@gmail.com>
2023-05-10 05:06:50 -0300

    test and it

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,22 +1,24 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import supertest from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('user can create a new transation', async () => {
-  await supertest(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 100,
-      type: 'credit',
-    })
-    .expect(201)
+  it('should be able to crate a new transaction', async () => {
+    await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 100,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
