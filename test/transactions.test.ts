5ac5c2ceb132103636760ad331a2c283d83fbc52
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-11 09:06:31 -0300

    test: get summary

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -82,4 +82,34 @@ describe('Transactions routes', () => {
       }),
     )
   })
+
+  it('should be able to get the summary', async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await supertest(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2500,
+        type: 'debit',
+      })
+
+    const summaryResponse = await supertest(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 2500,
+    })
+  })
 })
